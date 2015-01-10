43bcff1b9298a63341bf175b9864c0e10a6edcbf
Tangent 128 <Tangent128@gmail.com>
2015-01-10 17:57:42 -0500

    * add & test iteration

--- js/entity.js
+++ js/entity.js
@@ -2,34 +2,43 @@ var _5gon = _5gon || [];
 _5gon.push(function(loaded) {
 
     var idCounter = 0;
-           
-    function EntitySet() {
-	var self = this;
-	var memberSet = {};
-	var entityList = [];
 
-	this.add = function(entity) {
-	    if ( ! memberSet[entity.id]) {
-		memberSet[entity.id] = true;
-		entityList.splice(entityList.length, 0, entity);
-	    }
+    loaded("$").then(function($) {
+	function EntitySet() {
+	    var self = this;
+	    var memberSet = {};
+	    var entityList = [];
+
+	    this.add = function(entity) {
+		if ( ! memberSet[entity.id]) {
+		    memberSet[entity.id] = true;
+		    entityList.splice(entityList.length, 0, entity);
+		}
+	    };
+
+	    this.remove = function(entity) {
+		if (memberSet[entity.id]) {
+		    delete memberSet[entity.id];
+		    entityList.splice(entityList.indexOf(entity), 1);
+		}
+	    };
+	    
+	    this.each = function(callback) {
+		$.each(entityList, function() {
+		    callback(this);
+		});
+	    };
+
 	};
-
-	this.remove = function(entity) {
-	    if (memberSet[entity.id]) {
-		delete memberSet[entity.id];
-		entityList.splice(entityList.indexOf(entity), 1);
-	    }
-	};
-
-    }
     
-    function Entity() {
-	this.id = idCounter++;
-    };
+	function Entity() {
+	    this.id = idCounter++;
+	};
 
-    /* Exports */
-    loaded("EntitySet").resolve(EntitySet);
-    loaded("Entity").resolve(Entity);
+	/* Exports */
+	loaded("EntitySet").resolve(EntitySet);
+	loaded("Entity").resolve(Entity);
 
+    });
+    
 });
