d74b38303828724fd67d06bff9af337f9fbeceac
Archen@Keyboard <druddigoncuddlepile@gmail.com>
2015-01-14 20:52:09 -0800

    added velocity and velocity application

--- js/fireworks.js
+++ js/fireworks.js
@@ -4,6 +4,8 @@ _5gon.push(function(loaded) {
 	function SparkRenderSystem(set, cx) {
 		set.each(function(spark) {
 			cx.fillStyle = "#f00";
+            spark.location.x += spark.velocity.x;
+            spark.location.y += spark.velocity.y;
 			cx.fillRect(spark.location.x, spark.location.y, 16, 16);
 		});
 	};
