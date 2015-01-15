fd11ae5265971e876478ef7643a439530afd0d87
Archen@Keyboard <druddigoncuddlepile@gmail.com>
2015-01-14 21:22:23 -0800

    separated physics module from renderer

--- js/fireworks.js
+++ js/fireworks.js
@@ -1,19 +1,24 @@
 var _5gon = _5gon || [];
 _5gon.push(function(loaded) {
 	
+    function SparkPhysicsSystem(set) {
+        set.each(function(spark) {
+           spark.location.x += spark.velocity.x;
+           spark.location.y += spark.velocity.y;
+        });
+    };
+                 
 	function SparkRenderSystem(set, cx) {
 		set.each(function(spark) {
 			cx.fillStyle = "#f00";
-            spark.location.x += spark.velocity.x;
-            spark.location.y += spark.velocity.y;
 			cx.fillRect(spark.location.x, spark.location.y, 16, 16);
 		});
 	};
 	
 	/* Exports */
 	loaded("Fireworks").resolve({
-		SparkRenderSystem: SparkRenderSystem
-	});
-
+                                SparkRenderSystem: SparkRenderSystem,
+                                SparkPhysicsSystem: SparkPhysicsSystem
+                                       });
 	
 });
