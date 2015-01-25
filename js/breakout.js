7e573ba0330b6ea665dc62da5c5b642011c58b87
Archen@Keyboard <druddigoncuddlepile@gmail.com>
2015-01-25 15:39:23 -0800

    Updated stub constuctors

--- js/breakout.js
+++ js/breakout.js
@@ -1,35 +1,17 @@
 var _5gon = _5gon || [];
 _5gon.push(function(loaded) {
-
-           function Sprite(resource) {
-                this.sprite = resource;
-           };
            
            function Color(r, g, b) {
-                this.color = {};
-                this.color.r = r;
-                this.color.g = g;
-                this.color.b = b;
-           };
-           
-           function RemainingHits(remainingHits) {
-                this.remainingHits = remainingHits;
+                this.r = r;
+                this.g = g;
+                this.b = b;
            };
            
            function CollisionRectangle(x, y, w, h) {
-                this.collisionRectangle = {};
-                this.collisionRectangle.x = x;
-                this.collisionRectangle.y = y;
-                this.collisionRectangle.w = w;
-                this.collisionRectangle.h = h;
-           }
-           
-           function UserControlledPaddle() {
-                this.UserControlledPaddle = true;
-           }
-           
-           function IsABall() {
-                this.isABall = true;
+                this.x = x;
+                this.y = y;
+                this.w = w;
+                this.h = h;
            }
            
            function Mode(mode) {
