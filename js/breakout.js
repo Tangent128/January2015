6928a7de2fe7ce7f4b839a651c8d7cd477a2c1db
Archen@Keyboard <druddigoncuddlepile@gmail.com>
2015-01-25 15:33:32 -0800

    Added stub constructors to breakout.js

--- js/breakout.js
+++ js/breakout.js
@@ -1,6 +1,41 @@
 var _5gon = _5gon || [];
 _5gon.push(function(loaded) {
 
+           function Sprite(resource) {
+                this.sprite = resource;
+           };
+           
+           function Color(r, g, b) {
+                this.color = {};
+                this.color.r = r;
+                this.color.g = g;
+                this.color.b = b;
+           };
+           
+           function RemainingHits(remainingHits) {
+                this.remainingHits = remainingHits;
+           };
+           
+           function CollisionRectangle(x, y, w, h) {
+                this.collisionRectangle = {};
+                this.collisionRectangle.x = x;
+                this.collisionRectangle.y = y;
+                this.collisionRectangle.w = w;
+                this.collisionRectangle.h = h;
+           }
+           
+           function UserControlledPaddle() {
+                this.UserControlledPaddle = true;
+           }
+           
+           function IsABall() {
+                this.isABall = true;
+           }
+           
+           function Mode(mode) {
+                this.mode = mode;
+           }
+           
            loaded("Entities").then(function(Entities) {
             };
            
