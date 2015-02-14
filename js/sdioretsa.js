c1c0f5d8b5bc21d42876ea28471099d52b1c68cc
Tangent 128 <Tangent128@gmail.com>
2015-02-13 21:55:21 -0500

    * add skeleton for importing Sdioretsa code

---
+++ js/sdioretsa.js
@@ -0,0 +1,17 @@
+var _5gon = _5gon || [];
+_5gon.push(function(loaded) {
+
+	/* Components */
+	
+	/* Helper Functions */
+
+	/* State Objects */
+	
+	/* Systems */
+	
+	/* Exports */
+
+	loaded("sdioretsa").resolve({
+		
+	});
+});
