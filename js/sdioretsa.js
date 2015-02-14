262d130cdb35d3ad1179a7625a7bbc4ddf7b4880
Archen@Keyboard <druddigoncuddlepile@gmail.com>
2015-02-13 20:35:49 -0800

    adding components to sdioresta port

--- js/sdioretsa.js
+++ js/sdioretsa.js
@@ -52,9 +52,73 @@ _5gon.push(function(loaded) {
 	function RenderSystem(set) {
 	};
 	
+    function Sprite(img, size, angle) {
+        this.img = img;
+        this.size = size;
+        this.angle = 0;
+    }
+           
+    function PhysicsObject(sprite) {
+        this.position = new Entities.Position(0,0);
+        this.velocity = new Entities.Velocity(0,0);
+        this.thrust = 0;
+        this.sprite = sprite;
+        this.gScale = 1;
+    };
+           
+    function Asteroid(objects, position) {
+        var asteroid = new PhysicsObject(resource.asteroid, 50);
+           
+        asteroid.position = position;
+        //asteroid.angle = rand(Math.PI * 2);
+        asteroid.velocity.x = Math.cos(asteroid.angle) * 100;
+        asteroid.velocity.y = Math.sin(asteroid.angle) * 100;
+           
+        asteroid.isAsteroid = true;
+           
+        objects.push(asteroid);
+    };
+           
+    function Ship(objects, position, sprite, ai) {
+        var ship = new PhysicsObject(sprite);
+           
+        ship.position = position;
+        //ship.sprite.angle = rand(Math.PI * 2);
+        ship.velocity.x = 0;
+        ship.velocity.y = 0;
+        ship.think = ai;
+        ship.gScale = 0;
+           
+        ship.cooldown = 0;
+           
+        ship.isShip = true;
+           
+        objects.push(ship);
+           
+        return ship;
+    }
+           
+           
+    function spawnBullet(objects, position, sprite, angle) {
+        var bullet = new PhysicsObject(sprite);
+        bullet.position = position;
+        // This will now be retrieved from the sprite
+        //bullet.angle = angle;
+        bullet.velocity.x = 75 * Math.sin(angle);
+        bullet.velocity.y = 75 * Math.cos(angle);
+        bullet.think = bulletThink;
+        bullet.gScale = 0;
+           
+        bullet.isBullet = true;
+           
+        bullet.lifespan = 3.5;
+           
+    	objects.push(bullet);
+    }
+           
 	/* Exports */
 
 	loaded("sdioretsa").resolve({
-		
+           
 	});
 });
