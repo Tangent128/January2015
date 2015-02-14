79fc905395e5e278de91c51d8a2a6585583f71c2
Tangent 128 <Tangent128@gmail.com>
2015-02-13 23:16:07 -0500

    * fill in expected systems to be needed

--- js/sdioretsa.js
+++ js/sdioretsa.js
@@ -7,7 +7,44 @@ _5gon.push(function(loaded) {
 
 	/* State Objects */
 	
+	function GameState() {
+		this.state = "start"; // start, playing, won, lost
+	};
+	
+	function MessageHolder($wrapper, $text) {
+		this.$wrapper = $wrapper; // DOM element representing entire textbox
+		this.$text = $text; // DOM element whose contents are the displayed text
+		this.displaying = false;
+	};
+	
 	/* Systems */
+	
+	function GameResetSystem(set) {
+	};
+	
+	function ThrustSystem(set) {
+	};
+	
+	function GravitySystem(targetSet, sourceSet, G) {
+	};
+	
+	function WrapSystem(set) {
+	};
+	
+	function GravityWellControlSystem(set) {
+	};
+	
+	function EnemyAiSystem(shipSet, asteroidSet) {
+	};
+
+	function GameWinLossSystem(set, messageBox, winMessage, loseMessage) {
+	};
+
+	function UpdateSpriteFromPhysicsSystem(set) {
+	};
+	
+	function RenderSystem(set) {
+	};
 	
 	/* Exports */
 
